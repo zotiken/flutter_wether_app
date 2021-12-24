8ea6a7ef6acb076f157fa0f7569a308a0e51fda1
Nikita Lelyavin <nikitalelyavin@Nikitas-MacBook-Air.local>
2021-12-24 15:30:05 +0300

    add model for surealize

--- lib/model/weather_city.dart
+++ lib/model/weather_city.dart
@@ -5,7 +5,8 @@ part 'weather_city.g.dart';
 @JsonSerializable()
 class WeatherCity {
   City city;
-  WeatherCity({required this.city});
+  List<Day> list;
+  WeatherCity({required this.city, required this.list});
   factory WeatherCity.fromJson(Map<String, dynamic> json) =>
       _$WeatherCityFromJson(json);
 }
@@ -24,4 +25,45 @@ class City {
       required this.population,
       required this.timezone});
   factory City.fromJson(Map<String, dynamic> json) => _$CityFromJson(json);
+}
+
+@JsonSerializable()
+class Day {
+  final int dt,
+      sunrise,
+      sunset,
+      pressure,
+      humidity,
+      clouds,
+      speed,
+      deg,
+      gust,
+      pop;
+  final List<Temp> temp;
+  Day(
+      {required this.dt,
+      required this.clouds,
+      required this.deg,
+      required this.humidity,
+      required this.pop,
+      required this.gust,
+      required this.pressure,
+      required this.speed,
+      required this.sunrise,
+      required this.sunset,
+      required this.temp});
+  factory Day.fromJson(Map<String, dynamic> json) => _$DayFromJson(json);
+}
+
+@JsonSerializable()
+class Temp {
+  final int day, min, max, night, eve, morn;
+  Temp(
+      {required this.day,
+      required this.min,
+      required this.max,
+      required this.night,
+      required this.eve,
+      required this.morn});
+  factory Temp.fromJson(json) => _$TempFromJson(json);
 }
--- lib/model/weather_city.g.dart
+++ lib/model/weather_city.g.dart
@@ -8,11 +8,15 @@ part of 'weather_city.dart';
 
 WeatherCity _$WeatherCityFromJson(Map<String, dynamic> json) => WeatherCity(
       city: City.fromJson(json['city'] as Map<String, dynamic>),
+      list: (json['list'] as List<dynamic>)
+          .map((e) => Day.fromJson(e as List<dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$WeatherCityToJson(WeatherCity instance) =>
     <String, dynamic>{
       'city': instance.city,
+      'list': instance.list,
     };
 
 City _$CityFromJson(Map<String, dynamic> json) => City(
@@ -29,4 +33,51 @@ Map<String, dynamic> _$CityToJson(City instance) => <String, dynamic>{
       'country': instance.country,
       'population': instance.population,
       'timezone': instance.timezone,
+    };
+
+Day _$DayFromJson(Map<String, dynamic> json) => Day(
+      dt: json['dt'] as int,
+      clouds: json['clouds'] as int,
+      deg: json['deg'] as int,
+      humidity: json['humidity'] as int,
+      pop: json['pop'] as int,
+      gust: json['gust'] as int,
+      pressure: json['pressure'] as int,
+      speed: json['speed'] as int,
+      sunrise: json['sunrise'] as int,
+      sunset: json['sunset'] as int,
+      temp:
+          (json['temp'] as List<dynamic>).map((e) => Temp.fromJson(e)).toList(),
+    );
+
+Map<String, dynamic> _$DayToJson(Day instance) => <String, dynamic>{
+      'dt': instance.dt,
+      'sunrise': instance.sunrise,
+      'sunset': instance.sunset,
+      'pressure': instance.pressure,
+      'humidity': instance.humidity,
+      'clouds': instance.clouds,
+      'speed': instance.speed,
+      'deg': instance.deg,
+      'gust': instance.gust,
+      'pop': instance.pop,
+      'temp': instance.temp,
+    };
+
+Temp _$TempFromJson(Map<String, dynamic> json) => Temp(
+      day: json['day'] as int,
+      min: json['min'] as int,
+      max: json['max'] as int,
+      night: json['night'] as int,
+      eve: json['eve'] as int,
+      morn: json['morn'] as int,
+    );
+
+Map<String, dynamic> _$TempToJson(Temp instance) => <String, dynamic>{
+      'day': instance.day,
+      'min': instance.min,
+      'max': instance.max,
+      'night': instance.night,
+      'eve': instance.eve,
+      'morn': instance.morn,
     };
